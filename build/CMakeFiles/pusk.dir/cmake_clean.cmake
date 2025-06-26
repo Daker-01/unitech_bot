fd0be760b5c04b2dc5947792bc005769c6840c43
Далер Иматшоев <dalerimatsoev@iTerm.local>
2025-06-26 21:47:19 +0300

    update

---
+++ build/CMakeFiles/pusk.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pusk.dir/main.cpp.o"
+  "CMakeFiles/pusk.dir/main.cpp.o.d"
+  "pusk"
+  "pusk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pusk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
